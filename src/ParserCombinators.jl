989eb3bca805679094f6af33c49befb610e64857
Evan Reierson <evan.reierson@gmail.com>
2024-05-04 11:04:41 -0600

    started testing basic combinators

--- src/ParserCombinators.jl
+++ src/ParserCombinators.jl
@@ -1,5 +1,103 @@
 module ParserCombinators
 
-# Write your package code here.
+# Base combinators
+
+
+function then(parser1, parser2)
+    function (input::String)
+        res = parser1(input)
+
+        if isnothing(res)
+            nothing
+        else
+            (parsed1, rest1) = res
+            (parsed2, rest2) = parser2(rest1)
+            (string(parsed1, parsed2), rest2)
+        end
+    end
+end
+
+function or(parser1, parser2)
+    function (input::String)
+        res = parser1(input)
+
+        if isnothing(res)
+            parser2(input)
+        else
+            (parsed1, rest1) = res
+            (parsed1, rest1)
+        end
+    end
+end
+
+function drop_first(parser1, parser2)
+    function (input::String)
+        res = parser1(input)
+
+        if isnothing(res)
+            nothing
+        else
+            (_parsed1, rest1) = res
+            parser2(rest1)
+        end
+    end
+end
+
+function map(func, parser)
+    function (input::String)
+        res = parser(input)
+
+        if isnothing(res)
+            nothing
+        else
+            (parsed1, rest1) = res
+            (func(parsed1), rest1)
+        end
+    end
+end
+
+
+# Derived combinators
+function one_of(parsers)
+    foldl(or, parsers)
+end
+
+function sequence(parsers)
+    foldl(then, parsers)
+end
+
+function many(parser)
+
+    function many_rec(parser, input, output)
+        res = parser(input)
+
+        if isnothing(res)
+            (output, input)
+        else
+            (parsed, rest) = res
+            many_rec(parser, rest, string(output, parsed))
+        end
+    end
+
+    function (input::String)
+        many_rec(parser, input, "")
+    end
+end
+
+function some(parser)
+    then(parser, many(parser))
+end
+
+
+# Parsers
+function char_parser(char::Char)
+    function (input::String)
+        if input[1] == char
+            (char, input[2:end])
+        else
+            nothing
+        end
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,37 @@
 using ParserCombinators
 using Test
 
-@testset "ParserCombinators.jl" begin
-    # Write your tests here.
+@testset "Parse nested Vector with various data types" begin
+    input = "[234, 453.34, 'hello', [ [ 1, 2 ], 5.0 , 6 ], 'world' ]"
+    expected = [234, 453.34, "hello", [[1, 2], 5.0, 6], "world"]
+
+
+    digit_parsers = Base.map(ParserCombinators.char_parser, '0':'9')
+    digit = ParserCombinators.one_of(digit_parsers)
+
+    numbers = ParserCombinators.some(digit)
+
+    int = ParserCombinators.map(p -> parse(Int64, p), numbers)
+
+    dot = ParserCombinators.char_parser('.')
+    # exception on int :thinking:
+    float = ParserCombinators.map(p -> parse(Float64, p), ParserCombinators.then(ParserCombinators.then(numbers, dot), numbers))
+
+    number = ParserCombinators.or(float, int)
+
+
+
+end
+
+
+@testset "Parse to struct" begin
+    input = "(1 2) (30 40) (500 600)"
+
+    struct Point
+        x::Int
+        y::Int
+    end
+
+    expected = [Point(1, 2), Point(30, 40), Point(x=500, y=600)]
+
 end
